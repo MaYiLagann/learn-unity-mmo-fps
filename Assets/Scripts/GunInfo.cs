3cec9db2fadddce74a77d733c2dd55a143e6e6f1
MaYiLagann <lhwshy@naver.com>
2021-10-09 16:09:15 +0900

    Add item, gun info script

---
+++ Assets/Scripts/GunInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Gun information
+/// </summary>
+[CreateAssetMenu(menuName = "FPS/Gun")]
+public class GunInfo : ItemInfo
+{
+    
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item
+/// </summary>
+public class Item : MonoBehaviour
+{
+    /// <summary>
+    /// Item information
+    /// </summary>
+    public ItemInfo itemInfo;
+    /// <summary>
+    /// GameObject for item
+    /// </summary>
+    public GameObject itemGameObject;
+}
