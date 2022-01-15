f7b411d76c60bb4e72791e9036e22fc4138c82e3
MaYiLagann <lhwshy@naver.com>
2022-01-15 15:03:27 +0900

    Add RPC method 'Shoot' with hit position

--- Assets/Scripts/SingleShotGun.cs
+++ Assets/Scripts/SingleShotGun.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 /// <summary>
 /// Single shot gun item
 /// </summary>
+[RequireComponent(typeof(PhotonView))]
 public class SingleShotGun : Gun
 {
     /// <summary>
     /// Camera from player
     /// </summary>
     [SerializeField] new Camera camera;
+
+    /// <summary>
+    /// Photon view
+    /// </summary>
+    PhotonView photonView;
+
+
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded
+    /// </summary>
+    void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
 
 
@@ -34,6 +51,13 @@ public class SingleShotGun : Gun
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamagable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            photonView.RPC("RpcShoot", RpcTarget.All, hit.point);
         }
+    }
+
+    [PunRPC]
+    private void RpcShoot(Vector3 hitPosition)
+    {
+        Debug.Log(hitPosition);
     }
 }
